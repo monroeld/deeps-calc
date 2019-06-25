98fbbd18eb7e2fedd84e8dcad82cc4f6f96ddc36
Luke Monroe <ldmonroe80@gmail.com>
2019-06-25 15:47:48 -0700

    Working dataframe!

--- loot-tetris.R
+++ loot-tetris.R
@@ -67,50 +67,93 @@ data.raw <- rawToChar(data$content)
 # data.raw2 <- gsub("[\t]+", "\t", data.raw)
 data.rawsplit <- unlist(strsplit(data.raw, "\t"))
 data.rawsplit <- gsub("<(.|\n)*?>","",data.rawsplit)
-test <- data.rawsplit[grep("Special Abilities", data.rawsplit)]
-test2 <- gsub("\n", "@", test)
-test2 <- substr(test2, el(gregexpr("Item", test2))[1],
-                el(gregexpr("The above was automatically compiled", test2))[1]-1)
+fulltable<- data.rawsplit[grep("Special Abilities", data.rawsplit)]
+fulltable <- gsub("\n", "@", fulltable)
+fulltable <- substr(fulltable, el(gregexpr("Item", fulltable))[1],
+                el(gregexpr("The above was automatically compiled", fulltable))[1]-1)
 
 
-test3 <- data.frame(do.call('rbind', data.frame(unlist(strsplit(test2, "@{3,}")))))
-columnnames <- c(unlist(strsplit(test3[1, 1], "@@")))
+fulltable <- data.frame(do.call('rbind', data.frame(unlist(strsplit(fulltable, "@{3,}")))))
+columnnames <- c(unlist(strsplit(fulltable[1, 1], "@@")))
 names(columnnames) = NULL
-test3 <- test3[1, 2:ncol(test3)]
-
-# t4 <- test3[1, c(1:405)%%3-2 == 0]
-# t5 <- test3[1, c(1:405)%%3-1 == 0]
-# t6 <- test3[1, c(1:405)%%3 == 0]
-
-# test4 <- data.frame(cbind(t(t5), t(t6), t(t4)))
-
-nones <- which(c(unlist(gregexpr("^ None@@", test3))) > 0)
-mls <- which(c(unlist(gregexpr("^ [0-9]+@@", test3))) > 0)
-mls <- c(nones, mls)
-
-MLs <- test3[1, mls]
-
-working <- which(c(unlist(regexpr(":", test3[1, ]))) > 0)
-
-Effects <- test3[1, setdiff(working, mls)]
+fulltable <- fulltable[1, 2:ncol(fulltable)]
 
 
-Eff2 <- as.data.frame(t(Effects))
-colnames(Eff2) <- "Effs"
+mlInds <- which(c(unlist(gregexpr("(^ None@@)|(^ [0-9]+@@)", fulltable)))>0)
+
+
+# Effects have colons
+colons <- grep(":", fulltable[1, ])
+Effects <- fulltable[1, setdiff(colons, mlInds)]
+Effects <- t(Effects)
+
+
 
 
 # Concatenate all "Tier 3" effects into the columns with "Tier 1 & Tier 2" effects
-upp3 <- which(c(unlist(regexec("Upgradeable - Tier 3", as.character(Effects)))) == 2)
-Effects2 <- data.frame(cbind(t(Effects[, upp3-1]), t(Effects[, upp3])))
+upp1 <- which(c(unlist(regexec("Upgradeable - Tier 1", Effects))) == 2)
+upp3 <- which(c(unlist(regexec("Upgradeable - Tier 3", Effects))) == 2)
+Effects2 <- data.frame(cbind(Effects[upp3-1], Effects[upp3]))
 colnames(Effects2) <- c("E12", "E3")
 Effects2 <- mutate(Effects2, Eall = paste0(E12, E3))
-Effects2$Effs[match(Effects2$E12, Effects2$Effs)] <- Effects2$Eall
-Effects3 <- data.frame(t(Effects[1, ])[-c(which(startsWith(t(Effects[1, ]), " Upgradeable - Tier 3"))), ])
+
+
+Effects[upp1] <- Effects2$Eall
+Effects <- Effects[-c(upp3)]
+
+nEffects <- lengths(str_split(Effects, "@@"))
+allEffects <- c(unlist(str_split(Effects, "@@")))
+
+
+# Clean this up a bit
+MLs <- as.character(fulltable[1, mlInds])
+MLs <- trimws(substr(MLs, 0, c(unlist(regexpr("@@", MLs)))-1), which = "left")
+MLs <- as.numeric(gsub("None", 0, MLs))
+MLs <- rep(MLs, times = nEffects)
+
+# Names never have colons in them!
+gearNames <- as.character(fulltable[1, setdiff(c(1:ncol(fulltable)),
+                                               colons)])
+gearNames <- rep(gearNames, times = nEffects)
+
+
+gloves <- data.frame(cbind(gearNames, MLs, allEffects))
+
+
+# Working dataset complete!  Time for some dplyr
+gloves$augs <- unlist(regexec("Augment Slot", gloves$allEffects))
+gloves$feats <- unlist(regexec("[a-z]Feat:", gloves$allEffects))
+gloves$charges <- unlist(regexec("Charges:", gloves$allEffects))
+
+gloves$upgrades <- unlist(regexec("^ Upgradeable", gloves$allEffects))
+gloves$historic <- unlist(gregexpr("(historic)", gloves$gearNames))
+
+# Deduplicate crystal cove gear
+
+# for dedup
+gloves$colons <- unlist(regexec(":", gloves$allEffects))
+gloves <- gloves %>% mutate(allEffects = ifelse(augs > 0,
+                           substr(allEffects, 0, augs+11),
+                          allEffects)) %>%
+                    filter(upgrades < 0, historic < 0) %>%
+                    mutate(allEffects = ifelse(feats > 0,
+                            substr(allEffects, 0, feats),
+                          allEffects))
+
+test <- gloves %>% mutate(allEffects = ifelse(charges+feats+augs < 0,
+                                              substr(allEffects, 0, colons),
+                                              allEffects))
+
+test <- gloves %>% str_which(allEffects, "Feat:")
+  
+  mutate(Effs = ifelse(str_which(allEffects, "Feat:"), 1, 0))
 
 
 
-allEffs <- lapply(Effects3[, 1], function(x) c(unlist(str_split(x, "@{2,}"))))
-Effects3$splits <- allEffs
+
+# Before we do something crazy like below, see if we can get the other columns as vectors
+  # If so, can we repeat each one by the lengths of splits' sub-arrays and bind those?
+
 
 # Might need to change Effects3's elements to arrays now
 test2 <- lapply(Effects3$splits, function(x)
@@ -155,30 +198,10 @@ test2 <- lapply(Effects3$splits, function(x)
 
 
 
-# This works on a dummy vector so far
-# Substring at ":" or "Feat:"
-test2 <- sapply(c(unlist(strsplit(Effects3[, 1], "@@"))), function(x)
-  ifelse(is.na(c(unlist(gregexpr("Feat:", x)))[2]),
-         substring(x, 0, c(unlist(regexpr(":", x)))),
-         substring(x, 0, c(unlist(gregexpr("Feat:", x)))[2])))
-names(test2) <- NULL
-
-# Remove left whitespace, final character
-test2 <- trimws(test2, which = "left")
-test2 <- substring(test2, 0, nchar(test2)-1)
-
-# Halve the string unless it starts with "Feat"
-test3 <- trimws(ifelse(startsWith(test2, "Feat"), test2,
-                substring(test2, 0, nchar(test2)/2)), which = "right")
 
 
 
 
-# Embed weapon effects in a vector instead of a character string
-#    Don't do this until the end because we effectively won't be able to mutate the product, I think...
-allEffs <- lapply(Eff2$Effs, function(x) c(unlist(str_split(x, "@{2,}"))))
-Eff2$splits <- allEffs
-allEffs <- list(c(unlist(strsplit(Eff2$Effs, "@{2,}"))))
 
 # This line cuts off anything after an augment, which does happen
 #   Run it later on allEffs
@@ -187,41 +210,4 @@ Augless <- mutate(Eff2, Effs = ifelse(c(unlist(regexpr("Augment Slot", Effs))) >
                                     Effs))
 
 
-#  Stepping back a minute, what's in the "href" tags?
-# testing <- which(regexpr("Special Abilities", data.rawsplit) > 0)
-# testing <- data.rawsplit[testing]
-# testing2 <- substr(testing, el(gregexpr("Item", testing))[1],
-#                 el(gregexpr("The above was automatically compiled", testing))[1]-1)
-# 
-# # Remove things between "a href" tags?
-# hrefstart <- c(unlist(gregexpr("<a href=", testing2)))
-# hrefend <- c(unlist(gregexpr("</a>", testing2)))
-# 
-# newstart <- c(1, hrefend+1)[1:length(hrefend)]
-# newend <- c(hrefstart-1)
-# 
-# testing4 <- sapply(length(newstart):1, function(x)
-#                   substr(testing2, newstart[x]+3, newend[x]))
 
-# Try gsubbing out tags now?  Didn't seem to work
-# testing5 <- gsub("<(.|\n)*?>","",testing4)
-# 
-
-
-
-
-# augslots <- which(c(unlist(regexec("Augment Slot", as.character(Effects[1, ])))) > 1)
-# noaugs <- Effects[1, ]
-# 
-# Augless <- data.frame(substr(as.character(Effects[1, augslots]), 0,
-#                   c(unlist(regexec("Augment Slot", as.character(Effects[1, augslots])))) +11))
-# augment <- data.frame(as.character(Effects)[which(c(unlist(gregexpr("Augment Slot", Effects[1, ]))) >= 0)])
-
-# noAt <- which(c(unlist(regexec("@", as.character(test3[1, ])))) == -1)
-# Names <- test3[1, setdiff(mls, noAt)]
-
-# colnames(test3) <- NULL
-# rownames(test3) <- NULL
-
-# test4 <- data.frame(t(rbind(test3, ceiling(as.numeric(cut(1:ncol(test3), breaks = ncol(test3)/3))))))
-# test5 <- spread(test4, key = "X2", value = "X1")
